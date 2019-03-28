aeb0de9240220e2196092de9085d542be099e551
WHITING, JEREMY <jeremy.whiting23@gmail.com>
2019-03-28 08:04:41 -0500

    server updates

--- express-app/server.js
+++ express-app/server.js
@@ -19,24 +19,43 @@ app.post("/ledger", (request, response) => {
   //return ledger entry and value and coins
 
   // Get the current valuation and coins owned
-  axios.get("http://5c992ab94236560014393239.mockapi.io/shintobank/1").then((ledgerGetResponse) => {
-    console.log("ledgerGetResponse:", ledgerGetResponse.data)
+  axios
+    .get("http://5c992ab94236560014393239.mockapi.io/shintobank/1")
+    .then((ledgerGetResponse) => {
+      console.log("ledgerGetResponse:", ledgerGetResponse.data)
 
-    value = parseInt(ledgerGetResponse.data.valuation)
-    coins = parseInt(ledgerGetResponse.data.coinbank)
-    console.log("Value:", value, "Coins:", coins)
+      value = parseInt(ledgerGetResponse.data.valuation)
+      coins = parseInt(ledgerGetResponse.data.coinbank)
+      console.log("Value:", value, "Coins:", coins)
 
-    // Based on action either add or submit
-    if (request.body.action === "Mined" || request.body.action === "Bought") {
-      value += parseInt(request.body.amount)
-      coins += parseInt(request.body.amount)
-    } else {
-      value -= parseInt(request.body.amount)
-      coins -= parseInt(request.body.amount)
-    }
-    // Update coinbank and valuation
-    return axios.put("http://5c992ab94236560014393239.mockapi.io/shintobank/1", { valuation: value, coinbank: coins })
-  })
+      // Based on action either add or submit
+      if (request.body.action === "Mined" || request.body.action === "Bought") {
+        value += parseInt(request.body.amount)
+        coins += parseInt(request.body.amount)
+      } else {
+        value -= parseInt(request.body.amount)
+        coins -= parseInt(request.body.amount)
+      }
+      console.log("After if/else", "Value:", value, "Coins:", coins)
+
+      // Update coinbank and valuation
+      return axios.put("http://5c992ab94236560014393239.mockapi.io/shintobank/1", { valuation: value, coinbank: coins })
+    })
+    .then((ledgerPutResponse) => {
+      console.log("ledgerPutResponse:", ledgerPutResponse.data)
+
+      // Post transaction to the ledger
+      postRequest = { ...request.body, valuation: value }
+      console.log("postRequest", request.body) // I AM HERE
+      return axios.post("http://5c992ab94236560014393239.mockapi.io/ledger", postRequest)
+    })
+    .then((ledgerPostResponse) => {
+      //Return the transaction and updated info
+      return response.json({ ...ledgerPostResponse.data, coinbank: coins })
+    })
+    .catch((error) => {
+      console.log("ERROR")
+    })
 })
 
 // SERVER LISTENING
